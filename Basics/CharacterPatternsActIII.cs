59fc0ae7a0ab27d5415a33ad9bc28edf78f4d46b
Dawid Dyl <dawid.dyl@microsoft.wsei.edu.pl>
2023-11-18 17:55:46 +0100

    Task CPTTRN3 (Character Patterns Act 3)

--- Basics/CharacterPatternsActIII.cs
+++ Basics/CharacterPatternsActIII.cs
@@ -20,9 +20,6 @@ namespace SPOJ.Basics
                 int rows = int.Parse(rowsColumnsArr[0]);
                 int columns = int.Parse(rowsColumnsArr[1]);
 
-
-
-
                 for (int q = 0; q < rows * 3 + 1; q++)
                 {
                     for (int j = 0; j < columns * 3 + 1; j++)
@@ -41,46 +38,8 @@ namespace SPOJ.Basics
                         }
                     }
                     Console.WriteLine();
-
                 }
                 Console.WriteLine();
-
-
-
-
-
-
-
-
-
-
-
-
-                //    for (int q = 1; q <= rows; q++)
-                //    {
-                //        for (int w = 1; w <= 4; w++)
-                //        {
-                //            Console.Write("*");
-                //        }
-                //        Console.WriteLine();
-
-                //        for (int j = 1; j <= 2; j++)
-                //        {
-                //            for (int k = 1; k <= 4; k++)
-                //            {
-                //                if (k == 1 || k == 4)
-                //                {
-                //                    Console.Write("*");
-                //                }
-                //                else
-                //                {
-                //                    Console.Write(".");
-                //                }
-                //            }
-                //            Console.WriteLine();
-                //        }
-                //    }
-                //}
             }
         Console.ReadKey();
         }  
